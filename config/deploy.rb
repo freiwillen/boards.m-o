46bd9f0460978e2377d2c3931a507bdf0cee2ee7
freiwillen <kostya.malinovskiy@gmail.com>
2014-05-16 23:11:30 +0300

    changed deployment server. removed rvm from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,18 +9,18 @@ server  "m-o.com.ua", :app, :web, :db, :primary => true                         
 
 set :user, 'freiwillen'
 set :use_sudo, false
-set :deploy_to, "/home/freiwillen/www/#{application}"
+set :deploy_to, "/home/deployer/#{application}"
 #set :deploy_via, :remote_cache
 set :keep_releases, 5
 #set :rvm_ruby_string, 'ree-1.8.7-2011.03@mcdim'
-set :rvm_ruby_string, 'ree-1.8.7-2011.03@billboards_new'
-set :rvm_type, :system
+#set :rvm_ruby_string, 'ree-1.8.7-2011.03@billboards_new'
+#set :rvm_type, :system
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
 after 'deploy:update_code', 'deploy:symlink_shared'
 #after 'deploy:update', 'rvm:use_gemset'
 
-require "rvm/capistrano"  
+#require "rvm/capistrano"  
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -37,17 +37,9 @@ namespace :deploy do
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     #run "ln -nfs #{shared_path}/config/setup_load_paths.rb #{release_path}/config/setup_load_paths.rb"
-    run "cp #{shared_path}/config/.rvmrc #{release_path}/.rvmrc"
+   # run "cp #{shared_path}/config/.rvmrc #{release_path}/.rvmrc"
     run "ln -nfs #{shared_path}/system #{release_path}/public/system"
     run "mkdir #{release_path}/tmp/offers"
   end
 
 end
-namespace :rvm do
-  task :use_gemset do
-    desc 'Trust rvmrc file'
-    run "rvm rvmrc trust #{current_release}"
-    #run 'rvm use ree-1.8.7-2011.03@mcdim'
-  end
-end
-
